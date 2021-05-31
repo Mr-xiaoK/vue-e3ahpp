368acfe15510032beb9d1c1c6671303fe4daff29
Mr-xiaoK <1210331797@qq.com>
2021-05-31 16:40:55 +0800

    add scroll text-ellipsis

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-x-scroll
+  x-scroll
  <div class="x_scroll">
  <div class="scroll_child"></div>
  <div class="scroll_child"></div>
@@ -7,6 +7,21 @@ x-scroll
  <div class="scroll_child"></div>
  <div class="scroll_child"></div>
  <div class="scroll_child"></div>
+ </div>
+
+  y-scroll
+ <div class="y_scroll">
+ <div class="scroll_child"></div>
+ <div class="scroll_child"></div>
+ <div class="scroll_child"></div>
+ <div class="scroll_child"></div>
+ <div class="scroll_child"></div>
+ <div class="scroll_child"></div>
+ </div>
+ text-ellipsis
+ <div class="text_ellipsis">
+ <p>单行省略单行省略单行省略单行省略单行省略</p>
+ <p>多行省略多行省略多行省略多行省略多行省略多行省略多行省略多行省略多行省略</p>
  </div>
 </template>
 
@@ -19,15 +34,45 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="less" scoped>
 .x_scroll{
-  width: 500px;
+  width: 400px;
   height: 100px;
   display: -webkit-box;
   overflow-x: auto;
+  margin: 0 auto; 
   div{
     width: 100px;
     height: 100px;
     background-color: #464464;
     margin-right: 10px;
+  }
+}
+.y_scroll{
+  width: 100px;
+  height: 200px;
+  overflow: hidden;
+  overflow-y: scroll;
+  margin: 0 auto; 
+  div{
+    width: 100px;
+    height: 100px;
+    background-color: #464464;
+    margin-bottom: 10px;
+  }
+}
+.text_ellipsis{
+  margin: 0 auto;
+  width: 200px;
+  height: 200px;
+  p:nth-child(1){
+    overflow: hidden;
+    text-overflow:ellipsis;
+    white-space: nowrap;
+  }
+  p:nth-child(2){
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 2;
+    overflow: hidden;
   }
 }
 </style>
